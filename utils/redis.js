64db4f48439c4e77f7bfd58ec5123e1a7033812f
M'mah Zombo <www.mariamzombo@gmail.com>
2023-10-24 13:33:16 +0000

    task1

---
+++ utils/redis.js
@@ -0,0 +1,56 @@
+/* eslint-disable linebreak-style */
+import redis from 'redis';
+
+class RedisClient {
+  constructor() {
+    this.client = redis.createClient();
+
+    this.client.on('error', (error) => {
+      console.error(`Redis Client Error: ${error}`);
+    });
+  }
+
+  isAlive() {
+    return !this.client.connected;
+  }
+
+  async get(key) {
+    return new Promise((resolve, reject) => {
+      this.client.get(key, (error, value) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(value);
+        }
+      });
+    });
+  }
+
+  async set(key, value, duration) {
+    return new Promise((resolve, reject) => {
+      this.client.setex(key, duration, value, (error) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve();
+        }
+      });
+    });
+  }
+
+  async del(key) {
+    return new Promise((resolve, reject) => {
+      this.client.del(key, (error, count) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(count);
+        }
+      });
+    });
+  }
+}
+
+const redisClient = new RedisClient();
+
+export default redisClient;
